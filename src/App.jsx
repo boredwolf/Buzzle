ff4adb44700e0328250ed3af17dcea63bc16f79b
Kevin de Villiers <78181056+KevindeVilliers@users.noreply.github.com>
2021-10-28 10:31:23 +0200

    Fetch api (#35)
    * Récup question API, afficher question, merge incorrect et correct answers, afficher answers
    
    * Changer nom composant + dependence
    
    * Correction retour PR

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Questions from './components/Questions';
@@ -9,28 +10,27 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Switch>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/questions">
-            <Questions />
-          </Route>
-          <Route path="/endgame">
-            <Endgame />
-          </Route>
-          <Route path="/scores">
-            <Scores />
-          </Route>
-        </Switch>
+        <Questions />
       </div>
+      <Switch>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/questions">
+          <Questions />
+        </Route>
+        <Route path="/endgame">
+          <Endgame />
+        </Route>
+        <Route path="/scores">
+          <Scores />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
-
-  
 }
 
 export default App;
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,3 +1,67 @@
-const Questions = () => {};
+import { React, useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+
+function Questions() {
+  const url = 'https://opentdb.com/api.php?amount=10';
+  const [questions, setQuestions] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [qInd, setQInd] = useState(0);
+
+  const loadQuestions = async () => {
+    let response = fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        insertCorr(
+          data.results[0].incorrect_answers,
+          data.results[0].correct_answer
+        );
+        setQuestions(data.results);
+        setLoaded(true);
+      });
+  };
+
+  function insertCorr(arr, corr) {
+    const randInd = Math.floor(Math.random() * 4);
+    arr.splice(randInd, 0, corr);
+  }
+
+  const handleButton = (e, ans) => {
+    e.preventDefault();
+    if (qInd + 1 < questions.length) {
+      insertCorr(
+        questions[qInd + 1].incorrect_answers,
+        questions[qInd + 1].correct_answer
+      );
+      setQInd(qInd + 1);
+    } else {
+      loadQuestions();
+      setQInd(0);
+    }
+  };
+
+  return (
+    <div className="Questions">
+      <Button variant="contained" onClick={loadQuestions}>
+        Play
+      </Button>
+      {loaded && (
+        <div>
+          <p className="Question">{questions[qInd].question}</p>
+          {questions[qInd].incorrect_answers.map((a) => {
+            return (
+              <Button
+                variant="outlined"
+                key={a}
+                onClick={(e) => handleButton(e, a)}
+              >
+                {a}
+              </Button>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default Questions;
