33058a20020d6cf46f2a1ec034d924430a0f6d7a
adeleguillem <82821791+adeleguillem@users.noreply.github.com>
2021-10-28 15:14:22 +0200

    US #20 Bouton Play lancement du jeu depuis la page welcome (#38)

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,3 +1,20 @@
-const Welcome = () => {};
+import React from 'react';
+import logo from '../assets/images/logo-violet.png';
+import { NavLink } from 'react-router-dom';
 
-export default Welcome;
+const Welcome = () => {
+  return (
+    <div className="welcome-container1">
+      <div className="welcome-container2">
+        <div className="welcome-container3">
+          <img src={logo} alt="Logo" className="welcome-logo" />
+        </div>
+        <div className="play-button-container">
+          <NavLink exact to="/home">
+            <button className="play-button">PLAY</button>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
