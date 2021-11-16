a2509f73c09918bf58c21a4c9245ae19e27fab68
Félix Jouvhomme <80586378+boredwolf@users.noreply.github.com>
2021-11-16 09:20:21 +0100

    Lalalalalalala (#85)
    * prout
    
    * idkplsletmecommit
    
    * lalalala

---
+++ src/components/Questionnaire.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Questionnaire = ({
+  showAnswers,
+  handleAnswer,
+  data: {  correct_answer, answers },
+}) => {
+
+  return (
+    <div>
+      
+      <div >
+       {answers.map((answer, idx) => {
+         const bgColor = showAnswers 
+         ? answer === correct_answer 
+         ? 'green-button' 
+         : 'red-button' 
+         : null;
+         
+         return (
+        <button
+        type = "button"
+        variant = "contained"
+        key={idx}
+          className={`${bgColor} response-button`}
+         onClick={() => handleAnswer(answer)}
+          dangerouslySetInnerHTML={{__html: atob(answer)}}
+          />
+          )})}
+      </div>
+     
+    </div>
+  );
+};
+
+export default Questionaire;
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -6,26 +6,48 @@ import PlayerInfos from "./PlayerInfos";
 import logo from "../assets/images/logo-violet.png";
 import Timer from "./Timer";
 import UrlContext from "../Contexts/UrlContext";
+import Questionnaire from "./Questionnaire";
 
 function Questions({ username, onScoreChange }) {
   const { url, setUrl } = useContext(UrlContext);
   const [questions, setQuestions] = useState([]);
   const [loaded, setLoaded] = useState(false);
   const [qInd, setQInd] = useState(0);
-  const [idName, setIdName] = useState();
   const [score, setScore] = useState(0);
-  const [counter, setCounter] = useState(200);
+  const [counter, setCounter] = useState(20);
   const [life, setLife] = useState(3);
   const history = useHistory();
-  const { difficulty, setDifficulty} = useContext(UrlContext);
-  const { category, setCategory } = useContext(UrlContext);
   const [answered, setAnswered] = useState(false);
+  const [showAnswers, setShowAnswers] = useState(false);
 
-  function insertCorr(arr, corr) {
-    const randInd = Math.floor(Math.random() * 4);
-    arr.splice(randInd, 0, corr);
-  }
+  const handleAnswer = (answer) => {
+    // set the counter and index
+    setTimeout(() => {
+      if (qInd + 1 < questions.length) {
+        setQInd(qInd + 1);
+      } else {
+        setQInd(1000);
+      }
+      setCounter(20);
+      setShowAnswers(false);
+    }, 5000);
 
+    if (!showAnswers) {
+      //Prevent double answers
+      //set score and life if correct answer or not
+      if (answer === questions[qInd].correct_answer) {
+        setScore(score + 100);
+        setCounter(5);
+      } else {
+        setCounter(5);
+        handleLife();
+        setAnswered(true);
+      }
+      setShowAnswers(true);
+    }
+  };
+
+  //function to handle the life
   function handleLife() {
     if (life > 1) {
       setLife(life - 1);
@@ -33,122 +55,92 @@ function Questions({ username, onScoreChange }) {
 
     if (life === 1) {
       setLife(life - 1);
-      history.push("/endgame");
+      setTimeout(() => {
+        history.push("/endgame");
+      }, 2000);
     }
   }
-
-  function onResponseClick(answer) {
-    setTimeout(() => {
-      if (qInd + 1 < questions.length) {
-        insertCorr(
-          questions[qInd + 1].incorrect_answers,
-          questions[qInd + 1].correct_answer
-        );
-        setAnswered(false)
-        setQInd(qInd + 1);
-      } else {
-        setQInd(1000);
-      }
-      setCounter(200);
-    }, 2000);
-
-  if (answer === questions[qInd].correct_answer) {
-      setIdName("green-button");
-      {
-      setScore(score + 100);
-      setAnswered(true);
-      }
-    }
-    else {
-        
-        setIdName("red-button");
-        handleLife();
-        setAnswered(true);
-        }}
 
   function onTimeout() {
-    onResponseClick("");
+    handleAnswer("");
   }
 
+  //fetching the api data
   useEffect(() => {
-    console.log(url)
+    console.log(url);
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        insertCorr(
-          data.results[0].incorrect_answers,
-          data.results[0].correct_answer
-        );
-        setQuestions(data.results);
+        const questions = data.results.map((question) => ({
+          ...question,
+          answers: [
+            question.correct_answer,
+            ...question.incorrect_answers,
+          ].sort(() => Math.random() - 0.5),
+        }));
+        setQuestions(questions);
         setLoaded(true);
       });
   }, []);
-
-  useEffect(() => {
-    setIdName();
-  }, [qInd]);
 
   useEffect(() => {
     onScoreChange(score);
   }, [score]);
 
   return (
-    <div className="questions-container">
-      <img className="logo logo-questions" src={logo} alt="logo Buzzle" />
-      <div className="questions">
-        {!loaded && <div className="loader" />}
-        {loaded && qInd < questions.length && (
-          <div className="q-and-a-container">
-            <div className="close-button-container">
-              <Timer
-                counter={counter}
-                setCounter={setCounter}
-                onTimeout={onTimeout}
-              />
-              <Link to="/Home">
-                <CancelRoundedIcon sx={{ fontSize: 40 }} />
-              </Link>
-            </div>
-            <div className="num-questions">
-              Question {qInd + 1} / {questions.length}
-            </div>
+    <div>
+      <div className="questions-container">
+        <img className="logo logo-questions" src={logo} alt="logo Buzzle" />
+        <div className="questions">
+          {!loaded && <div className="loader" />}
+          {loaded && qInd < questions.length && (
+            <div className="q-and-a-container">
+              <div className="close-button-container">
+                <Timer
+                  counter={counter}
+                  setCounter={setCounter}
+                  onTimeout={onTimeout}
+                />
+                <Link to="/Home">
+                  <CancelRoundedIcon sx={{ fontSize: 40 }} />
+                </Link>
+              </div>
+              <div className="num-questions">
+                Question {qInd + 1} / {questions.length}
+              </div>
 
-            <div className="container-questions-answers">
-              <h1
-                className="question"
-                dangerouslySetInnerHTML={{ __html: atob(questions[qInd].question) }}
-              />
+              <div className="container-questions-answers">
+                <h1
+                  className="question"
+                  dangerouslySetInnerHTML={{
+                    __html: atob(questions[qInd].question),
+                  }}
+                />
 
-              <div className="button-container">
-                {questions[qInd].incorrect_answers.map((answer) => (
-                  <button
-                    type="button"
-                    variant="contained"
-                    key={answer}
-                    onClick={() => {!answered ? onResponseClick(answer) : null}}
-                    className="response-button"
-                    id={idName}
-                  >
-                    {atob(answer)}
-                  </button>
-                ))}
+                <div className="button-container">
+                  <Questionaire
+                    data={questions[qInd]}
+                    showAnswers={showAnswers}
+                    handleAnswer={handleAnswer}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {loaded && qInd >= questions.length && (
-          <div className="end-button-container">
-            <NavLink to="/endgame">
-              <button type="button" className="end-button">
-                End of the Game
-              </button>
-            </NavLink>
-          </div>
-        )}
+          {loaded && qInd >= questions.length && (
+            <div className="end-button-container">
+              <NavLink to="/endgame">
+                <button type="button" className="end-button">
+                  End of the Game
+                </button>
+              </NavLink>
+            </div>
+          )}
+        </div>
+
+        <PlayerInfos username={username} score={score} life={life} />
       </div>
-
-      <PlayerInfos username={username} score={score} life={life} />
     </div>
   );
 }
